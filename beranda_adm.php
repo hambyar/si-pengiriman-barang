7be5cb42ced359d1791cb957cd70ad64a1ce3150
hambyar <78124051+hambyar@users.noreply.github.com>
2021-01-28 08:42:37 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Data Pengiriman Barang</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/j.png"></img></p>
+                          <p class="label-title" align="center"><strong>KISARAN</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                             Selamat datang di halaman administrator Sistem Informasi Pengiriman Barang Secara Cepat .<br>
+                            Melalui halaman ini dapat dilakukan &T Express adalah salah satu jasa pengiriman yang mampu melayani pengiriman dalam kota,<br>
+                             antar kota maupun antar provinsi.<br>
+                           J&T Express juga menyediakan layanan penjemputan ke alamat Pelapak.<br>
+                            Penjelasan cara aktivasi jasa pengiriman J&T Express dapat dibaca pada halaman<br><br><br>
+
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
